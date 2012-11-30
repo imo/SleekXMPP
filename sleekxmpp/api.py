b8766c5c8ac04653b073f9d34a10cacd695975c9
Adrian Sandor <adrian.sandor@imo.im>
2012-11-29 21:31:00 -0800

    removed old handler invocation because it hides errors

--- sleekxmpp/api.py
+++ sleekxmpp/api.py
@@ -129,13 +129,7 @@ class APIRegistry(object):
             handler = self._handlers[ctype][op].get('global', None)
 
         if handler:
-            try:
-                return handler(jid, node, ifrom, args)
-            except TypeError:
-                log.warn('handler failed, retrying without ifrom', exc_info=True)
-                # To preserve backward compatibility, drop the ifrom
-                # parameter for existing handlers that don't understand it.
-                return handler(jid, node, args)
+            return handler(jid, node, ifrom, args)
 
     def register(self, handler, ctype, op, jid=None, node=None, default=False):
         """Register an API callback, with JID+node specificity.
